91da5e88f7ca52c975ba007eff0d3f14b9dd85d9
Danyel-xp <danyellrodrigues023@gmail.com>
2026-09-15 14:36:58 -0300

    feat: Adicionar divisão

--- calculadora.py
+++ calculadora.py
@@ -1,35 +1,46 @@
-print("|==================================|\n");
-print("|            CALCULADORA           |\n");
-print("|==================================|\n");
-print("| 1. SOMA                          |\n");
-print("| 1. SUBTRAÇÃO                     |\n");
-print("| 3. MULTIPLICAÇÃO                 |\n");
-print("| 4. DIVISÃO                       |\n");
-print("| 5. SAIR                          |\n");
-print("|==================================|\n");
+def divisao():
+    a = float(input("Valor A: "))
+    b = float(input("Valor B: "))
+    
+    if b < 0 or b == 0 :
+        print("Não e possivel dividir por 0")
 
+    else:
+        divisao = a / b
+        print(f"===> Resultado: {divisao}")
 
 
 while  True: 
 
-    escolha = float(input("Esolha uma opção: "));
+    print("|==================================|\n")
+    print("|            CALCULADORA           |\n")
+    print("|==================================|\n")
+    print("| 1. SOMA                          |\n")
+    print("| 1. SUBTRAÇÃO                     |\n")
+    print("| 3. MULTIPLICAÇÃO                 |\n")
+    print("| 4. DIVISÃO                       |\n")
+    print("| 5. SAIR                          |\n")
+    print("|==================================|\n")
+
+
+    escolha = float(input("Esolha uma opção: "))
 
     if escolha == 5:
-        print("Encerrando programa..");
-        print("Programa encerrado com sucesso!");
-        exit();
+        print("Encerrando programa..")
+        print("Programa encerrado com sucesso!")
+        exit()
 
     elif escolha == 1:
-        #função soma
+        soma()
 
     elif escolha == 2:
-        #função de subtração
+        subtracao()
 
     elif escolha == 3:
-        #função de multiplicação
+        multiplicacao()
 
     elif escolha == 4:
-        #função de divisão
+        divisao()
 
     else:
-        print("Opção invalida! Tente novamente...");
+        print("Opção invalida! Tente novamente...")
