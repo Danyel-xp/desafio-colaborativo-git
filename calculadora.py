0c6e7f85db903f3ee522ecd35d55542eec4d78c1
Danyel-xp <danyellrodrigues023@gmail.com>
2026-09-18 00:32:54 -0300

    fix: corrigir modelo padrão

--- calculadora.py
+++ calculadora.py
@@ -1,39 +1,35 @@
-print("|==================================|\n");
-print("|            CALCULADORA           |\n");
-print("|==================================|\n");
-print("| 1. SOMA                          |\n");
-print("| 1. SUBTRAÇÃO                     |\n");
-print("| 3. MULTIPLICAÇÃO                 |\n");
-print("| 4. DIVISÃO                       |\n");
-print("| 5. SAIR                          |\n");
-print("|==================================|\n");
+print("|==================================|\n")
+print("|            CALCULADORA           |\n")
+print("|==================================|\n")
+print("| 1. SOMA                          |\n")
+print("| 2. SUBTRAÇÃO                     |\n")
+print("| 3. MULTIPLICAÇÃO                 |\n")
+print("| 4. DIVISÃO                       |\n")
+print("| 5. SAIR                          |\n")
+print("|==================================|\n")
 
 
 
 while  True: 
 
-    escolha = float(input("Esolha uma opção: "));
+    escolha = float(input("Esolha uma opção: "))
 
     if escolha == 5:
-        print("Encerrando programa..");
-        print("Programa encerrado com sucesso!");
-        exit();
+        print("Encerrando programa..")
+        print("Programa encerrado com sucesso!")
+        exit()
 
     elif escolha == 1:
-        #função soma
+        soma()
 
     elif escolha == 2:
-        num1 = float(input("Digite o primeiro valor: "))
-        num2 = float(input("Digite o segundo valor: "))
-        subtracao = num1 - num2
-        print(f"Resultade da subtração entre {num1} e {num2} é igual a: ", subtracao)
-        
+        subtracao()
 
     elif escolha == 3:
-        #função de multiplicação
+        multiplicacao()
 
     elif escolha == 4:
-        #função de divisão
+        divisao()
 
     else:
-        print("Opção invalida! Tente novamente...");
+        print("Opção invalida! Tente novamente...")
